c50db3a5446a23178de8266ce6d5c5fef73b9de1
felipe cook <felipepcook@gmail.com>
2022-03-19 19:29:33 -0400

    created playRound() func to facilitate round play

--- application.js
+++ application.js
@@ -5,3 +5,49 @@ function computerPlay() {
   return arr[choice];
 }
 
+function playRound(computerSelection, playerSelection) {
+  switch (computerSelection) {
+    case "rock":
+      if (playerSelection == "rock") {
+        console.log("You tie!");
+        return "You tie!";
+      } else if (playerSelection == "paper") {
+        console.log("You win!");
+        return "You win!";
+      } else {
+        console.log("You lose!");
+        return "You lose!";
+      }
+      break;
+    case "paper":
+      if (playerSelection == "rock") {
+        console.log("You lose!");
+        return "You lose!";
+      } else if (playerSelection == "paper") {
+        console.log("You tie!");
+        return "You tie!";
+      } else {
+        console.log("You win!");
+        return "You win!";
+      }
+      break;
+    case "scissors":
+      if (playerSelection == "rock") {
+        console.log("You win!");
+        return "You win!";
+      } else if (playerSelection == "paper") {
+        console.log("You lose!");
+        return "You lose!";
+      } else {
+        console.log("You tie!");
+        return "You tie!";
+      }
+      break;
+
+  }
+
+}
+
+const computerSelection = computerPlay();
+const playerSelection = prompt("Please enter Rock, Paper or Scissors: ").toLowerCase();
+
